abbba9c34654c854c243a46c57f94f4b4c6275c8
Patrik Reizinger <prmedia17@gmail.com>
2023-07-20 12:45:40 +0200

    test_model import and other fixes

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,18 +1,24 @@
-def test_rescaling_A():
-    # create an instance of the model with num_dim=3 and
+from lti_ica.models import LTINet
+import torch
+
+
+def test_rescaling_A(num_comp, num_segment):
+    # create an instance of the model with num_dim=num_comp and
     model = LTINet(
-        num_dim=3,
+        num_dim=num_comp,
+        num_class=num_segment,
     )
     # get the eigenvalues of A
     eigvals = torch.linalg.eig(model.A.weight.data)[0]
-    # check that all eigenvalues are less than 1
-    assert torch.all(eigvals < 1)
+    # check that all eigenvalues lie within the unit circle
+    assert torch.all(eigvals.abs() < 1)
 
 
-def test_input_shape():
-    # create an instance of the model with num_dim=3 and
+def test_input_shape(num_comp, num_segment):
+    # create an instance of the model with num_dim=num_comp and
     model = LTINet(
-        num_dim=3,
+        num_dim=num_comp,
+        num_class=num_segment,
     )
     # create a dummy input tensor with shape [batch, time(t:t-p), dim]
     x = torch.randn(5, 10, 3)
@@ -22,9 +28,9 @@ def test_input_shape():
     assert output.shape == (5, 3)
 
 
-def test_C_inv():
-    # create an instance of the model with num_dim=3 and , and C_inv set to False
-    model = LTINet(num_dim=3, C=False)
+def test_C_inv(num_comp, num_segment):
+    # create an instance of the model with num_dim=num_comp and , and C_inv set to False
+    model = LTINet(num_dim=num_comp, num_class=num_segment, C=False)
     # create a dummy input tensor with shape [batch, time(t:t-p), dim]
     x = torch.randn(5, 10, 3)
     # pass the input through the model
@@ -33,25 +39,9 @@ def test_C_inv():
     assert output.shape == (5, 3)
 
 
-def test_identity_matrix_B_inv():
-    # create an instance of the model with num_dim=3 and
-    model = LTINet(num_dim=3)
-    # check that B_inv is set to identity matrix
-    assert torch.allclose(torch.eye(model.num_dim), model.B_inv.weight)
-    # check that B_inv requires_grad is False
-    assert model.B_inv.weight.requires_grad == False
+def test_identity_matrix_B_inv(num_comp, num_segment):
+    # create an instance of the model with num_dim=num_comp and
+    model = LTINet(num_dim=num_comp, num_class=num_segment, B=False)
 
-
-def test_triangular():
-    # create an instance of the model with num_dim=3 and , and triangular set to True
-    model = LTINet(num_dim=3, triangular=True)
-    # create a dummy input tensor with shape [batch, time(t:t-p), dim]
-    x = torch.randn(5, 10, 3)
-    # pass the input through the model
-    output = model(x)
-    # check that the output shape is [batch, dim]
-    assert output.shape == (5, 3)
-
-    # check that all elements above the main diagonal are 0
-    A = model.A.weight.data
-    assert torch.triu(A, diagonal=1).sum() == 0
+    assert torch.allclose(torch.eye(model.num_dim), model.B_inv)
+    assert model.B_inv.requires_grad == False
