9d91356caa31729ece7b2ad8eee0fab84b61e85e
Patrik Reizinger <prmedia17@gmail.com>
2023-09-27 13:09:59 +0200

    runner fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
-from lti_ica.models import LTINet, LTINetMLP
-from lti_ica.datamodule import NonstationaryLTIDatamodule
 import torch
+
+from lti_ica.datamodule import NonstationaryLTIDatamodule
+from lti_ica.runner import LTILightning
 
 
 @pytest.fixture
@@ -34,16 +35,6 @@ def device():
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-# @pytest.fixture(use_B=[True, False]])
-# def lti_model(use_B):
-#     model = lti_ica.models.LTINet(num_dim=data.shape[-1],
-#                                   num_class=num_segment, C=False, triangular=triangular, B=use_B)
-#
-# @pytest.fixture
-# def lti_mlp_model():
-#     model = lti_ica.models.LTINetMLP(num_dim=data.shape[-1])
-
-
 @pytest.fixture
 def datamodule(num_comp, num_segmentdata, num_segment, dt):
     num_data = num_segmentdata * num_segment
@@ -51,3 +42,9 @@ def datamodule(num_comp, num_segmentdata, num_segment, dt):
     datamodule.setup()
 
     return datamodule
+
+
+@pytest.fixture
+def runner(num_comp, num_segmentdata, num_segment, dt):
+    num_data = num_segmentdata * num_segment
+    return LTILightning(num_comp, num_data, num_segment, dt)
