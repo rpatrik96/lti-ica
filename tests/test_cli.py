2a104ec32272d648ccf8a36fffd247f52099d74f
Patrik Reizinger <prmedia17@gmail.com>
2023-09-29 10:35:26 +0200

    config abs path for cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,16 @@
 from lti_ica.cli import LTILightningCLI
 from lti_ica.runner import LTILightning
 from lti_ica.datamodule import NonstationaryLTIDatamodule
+from os.path import abspath, dirname, join
 
 
 def test_cli_fast_dev_run():
+    config_path = join(dirname(dirname(abspath(__file__))), "configs", "config.yaml")
+
     args = [
         "fit",
         "--config",
-        "../configs/config.yaml",
+        config_path,
         "--trainer.fast_dev_run",
         "true",
     ]
